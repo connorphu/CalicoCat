c371edbaa748e29cd316850a48e33f2e1a68a4e3
connorphu <phuk0784@my.uwstout.edu>
2019-04-16 20:40:59 -0500

    OOP Concepts

---
+++ OOPConcepts/OOPConcepts/MyCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPConcepts
+{
+    class MyCalculator
+    {
+        // Declaring a function
+        // [scope] [return type] [function name](parameter)
+        /* 1. Scope:
+         * public - any class in any namespace can use
+         * private - only current class can use
+         * internal (default) - any class in the same namespace can use
+         */
+         /* 2. Return Type:
+          * any type that the function is expected to 'return'
+          * void - if nothing is being return
+          */
+         /* 3. Functions:
+          * parameters - things that the function expects for it to work (ie. 2 integers num1 and num2)
+          */
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+
+    }
+}
--- OOPConcepts/OOPConcepts/Program.cs
+++ OOPConcepts/OOPConcepts/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
+
 namespace OOPConcepts
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            
+            /* Types:
+             * int - integer numbers
+             * double - decimal numbers
+             * string - text (array of char)
+             * bool - true/false
+             * char - character (one letter)
+             * array [] - collection of objects
+             */
+
+            // Declaring variable
+            // [type] [name of variable]
+            int num;
+            num = 4;
+            int num3 = 27;
+            double num2 = 2.2;
+            Console.WriteLine(num2);
+            char char1 = 'c';
+            string word = "this is a string";
+
+            //Creating a MyCalculator object
+            MyCalculator myCalc = new MyCalculator();
+            Console.WriteLine(myCalc.Add(3, 5));
         }
     }
 }
